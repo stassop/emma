b7792cde62f4ff0f1035f396369618715d966c1a
Stanislav Sopov <stanislavsopov@stanislavs-mbp.home>
2022-01-31 19:59:04 +0100

    add unit tests

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,14 +1,37 @@
-/**
- * @format
- */
-
-import 'react-native';
+import { ScrollView, Image, Text } from 'react-native';
 import React from 'react';
-import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+import App from '../components/App';
+import Avatars from '../components/Avatars';
+import Descriptions from '../components/Descriptions';
+
+const usersMock = [{
+  name: "Tom Cruise",
+  title: "Actor",
+  image: "Tom Cruise.png",
+  description: "Thomas Cruise Mapother IV (born July 3, 1962) is an American actor and producer.",
+  id: "123",
+}]
+
+jest.mock('../data/users.json', () => usersMock);
+
+describe('App', () => {
+  it('renders correctly', () => {
+    var app = renderer.create(<App />).root;
+
+    expect(app.findAllByType(Avatars)).toHaveLength(1);
+    expect(app.findAllByType(Descriptions)).toHaveLength(1);
+
+    const images = app.findByType(Avatars).findByType(ScrollView).findAllByType(Image);
+    expect(images).toHaveLength(1);
+
+    const texts = app.findByType(Descriptions).findByType(ScrollView).findAllByType(Text);
+    expect(texts).toHaveLength(3);
+    expect(texts[0].props.children).toBe('Tom Cruise');
+    expect(texts[1].props.children).toBe('Actor');
+    expect(texts[2].props.children).toBe('Thomas Cruise Mapother IV (born July 3, 1962) is an American actor and producer.');
+  });
 });
